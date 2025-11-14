da8fa3a9a0795d147cc05cb307dc0bb0fa2b82c6
JOÃO VITOR DA ROSA MACHADO <joao.vitor23@univille.br>
2025-11-14 19:57:21 -0300

    Initial commit

---
+++ ClassFeedback/src/main/java/SpringAi/ClassFeedback/ClassFeedbackApplication.java
@@ -0,0 +1,13 @@
+package SpringAi.ClassFeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassFeedbackApplication.class, args);
+	}
+
+}
