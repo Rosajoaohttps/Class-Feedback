43bced10c8259fda7384d80f3d67cf11072c6fdf
1144007836 <gustavoalecxander9@gmail.com>
2025-11-21 19:40:24 -0300

    add index.html

--- src/main/java/com/Class_Feedback/Feedback/controller/HomeController.java
+++ src/main/java/com/Class_Feedback/Feedback/controller/HomeController.java
@@ -1,4 +1,10 @@
 package com.Class_Feedback.Feedback.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping()
 public class HomeController {
 }
